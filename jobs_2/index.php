ca979472293cda2ec865f954dbbb4c94cffeffac
Чужегин Андрей А. <achuzhegin@u-energo.ru>
2025-10-23 13:35:01 +0400

    X

--- jobs_2/index.php
+++ jobs_2/index.php
@@ -1,36 +1,36 @@
 <?php
-$num = 0;
-function showDataJobs(int $year, int $month, ?int $num)
+$nonWorkingDays = 0;
+function showDataJobs(int $year, int $month, int &$nonWorkingDays)
 {
-  $date = DateTime::createFromFormat("Y-m-d", "$year-$month-01"); 
+  $date = DateTime::createFromFormat("Y-m-d", "$year-$month-01");
   echo $date->format('F') . PHP_EOL;
   echo "-------------------------------" . PHP_EOL;
   $col = $date->format('t');
-  // print_r($col);
-  
+  //  print_r($col);
+
   do {
     $dayWeek = $date->format('w');
-    if ($dayWeek == '0') {
-      echo $date->format('d - D') . PHP_EOL;
-    } elseif ($num == 0) {
+    $nonWork = ($dayWeek == '0' || $dayWeek == '6'); 
+
+    if ($nonWork || $nonWorkingDays <= 2 && $nonWorkingDays > 0) {
+       echo $date->format('d - D') . PHP_EOL;
+       
+    }
+    else {
       echo "+" . ($date->format('d - D')) . PHP_EOL;
-      $num++;
-    } else {
-      echo $date->format('d - D') . PHP_EOL;
-      $num++;
+      $nonWorkingDays = 0;
     }
-    if ($num >= 3) {
-      $num = 0;
-    }
+    $nonWorkingDays++;
+    
+    
     $date->modify("+1 day");
     $col--;
   } while ($col > 0);
 }
 $year = 2025;
-$month = 10;
+$month = 1;
 $numMonth = 2;
 
-for ($i=$month; $i < $numMonth + $month; $i++) { 
-  showDataJobs($year, $i,$num);  
+for ($i = $month; $i < ($numMonth + $month); $i++) {
+  showDataJobs($year, $i, $nonWorkingDays);
 }
-
